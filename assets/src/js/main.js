a157c42e3fecd925cc152dba9607472ee5c2a7f5
itchief <alex.malcev1980@gmail.com>
2022-01-01 20:42:57 +1000

    Update

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,24 +1,8 @@
 // import jQuery
 // ../../../node_modules/jquery/dist/jquery.js
 
-// import Popper
-//= ../../../node_modules/@popperjs/core/dist/umd/popper.js
-
-// import required js-files Bootstrap 5
-//= ../../../node_modules/bootstrap/dist/js/bootstrap.js
-
-// ../../../node_modules/bootstrap/js/dist/alert.js
-// ../../../node_modules/bootstrap/js/dist/button.js
-// ../../../node_modules/bootstrap/js/dist/carousel.js
-// ../../../node_modules/bootstrap/js/dist/collapse.js
-// ../../../node_modules/bootstrap/js/dist/dropdown.js
-// ../../../node_modules/bootstrap/js/dist/modal.js
-// ../../../node_modules/bootstrap/js/dist/offcanvas.js
-// ../../../node_modules/bootstrap/js/dist/popover.js
-// ../../../node_modules/bootstrap/js/dist/scrollspy.js
-// ../../../node_modules/bootstrap/js/dist/tab.js
-// ../../../node_modules/bootstrap/js/dist/toast.js
-// ../../../node_modules/bootstrap/js/dist/tooltip.js
+// import js-file Popper & Bootstrap 5
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
 
 // import other js-files
 // my.js
